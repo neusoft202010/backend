b667188a7a1ab5512baf3343b12b633e77c378cf
王临轩 <728694781@qq.com>
2020-07-29 01:22:05 +0800

    项目第1次提交

---
+++ HospitalWebApp_Wwx/src/test/java/com/wwx/his/HospitalWebAppWwxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwx.his;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalWebAppWwxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
